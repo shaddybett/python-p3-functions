4f32ea3873d017adcaaba8faa51abef7e663c1f5
Shaddy <shadybett540@gmail.com>
2023-11-23 07:01:31 +0300

    fixed all errors

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python3
-
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+
+
+greet_programmer()    
 
 def greet(name):
-    pass
+    print(f'Hello, {name}!')
+
+
+greet('Bett')
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f'Hello, {name}!')
+
+
+greet_with_default('programmer')    
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+
+add(45,55) == 100    
 
 def halve(number):
-    pass
+    result = (int(number)/2)
+    print(result)
+    return result
+
+
+halve(100)    
